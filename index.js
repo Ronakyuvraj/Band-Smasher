be63b80fdccff5e1ae8f7e86d28d644f3425dacd
Being Yuvraj <ronak41294@gmail.com>
2025-01-20 15:25:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// var numOfDrumButtons = document.querySelectorAll(".drum").length;
+// ach
+// for(var i = 0;i<numOfDrumButtons;i++){
+// document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+// }
+// document.querySelectorAll(".drum").addEventListener("click", handleClick); 
+document.querySelectorAll(".drum").forEach(function (button) {
+    button.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+    var buttonInnerHtml = this.innerHTML;
+
+    keyPress(buttonInnerHtml);
+
+
+    // this.style.color = "";
+
+
+}
+document.addEventListener("keypress",function(event){
+   
+    keyPress(event.key);
+});
+function keyPress(key){
+
+    switch (key) {
+        case "w":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "a":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "s":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "d":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            // this.style.color = "white";
+
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "l":
+            // this.style.color = "white";
+// 
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+
+    }
+
+}
+
